c6c8dea04a5f62283672719475cb97024c408ab2
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-02-13 19:10:36 +0100

    Update base.py

--- konfusio/registry/base.py
+++ konfusio/registry/base.py
@@ -2,4 +2,5 @@ class BaseRegistry:
     name = "base"
 
     def exists(self, package: str) -> bool:
+        """Return True if the package exists in the registry"""
         raise NotImplementedError
