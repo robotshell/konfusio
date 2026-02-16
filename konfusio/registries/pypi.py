c83726e85c57d66bb889842d724298cc78773ff8
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-02-16 11:02:38 +0100

    Update pypi.py

--- konfusio/registries/pypi.py
+++ konfusio/registries/pypi.py
@@ -1,12 +1,6 @@
 import requests
-from .base import BaseRegistry
 
-class PyPIRegistry(BaseRegistry):
-    name = "pypi"
-
-    def exists(self, package):
-        try:
-            r = requests.get(f"https://pypi.org/pypi/{package}/json", timeout=5)
-            return r.status_code != 404
-        except:
-            return True
+def exists(pkg):
+    url = f"https://pypi.org/pypi/{pkg}/json"
+    r = requests.get(url, timeout=5)
+    return r.status_code == 200
