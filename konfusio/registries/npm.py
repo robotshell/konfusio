65c245e97c2af2c4914c66e3d680efc118ce359c
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-02-16 11:02:26 +0100

    Update npm.py

--- konfusio/registries/npm.py
+++ konfusio/registries/npm.py
@@ -1,8 +1,6 @@
 import requests
 
 def exists(pkg):
-    try:
-        r = requests.get(f"https://registry.npmjs.org/{pkg}", timeout=5)
-        return r.status_code != 404
-    except:
-        return True
+    url = f"https://registry.npmjs.org/{pkg}"
+    r = requests.get(url, timeout=5)
+    return r.status_code == 200
