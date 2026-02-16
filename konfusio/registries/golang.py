7072586e5b583e66553a8bbd68af40d14344a399
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-02-16 11:03:20 +0100

    Update golang.py

--- konfusio/registries/golang.py
+++ konfusio/registries/golang.py
@@ -1,13 +1,6 @@
 import requests
-from .base import BaseRegistry
 
-class GoRegistry(BaseRegistry):
-    name = "golang"
-
-    def exists(self, package):
-        try:
-            url = f"https://proxy.golang.org/{package}/@v/list"
-            r = requests.get(url, timeout=5)
-            return r.status_code != 404 and bool(r.text.strip())
-        except:
-            return True
+def exists(pkg):
+    url = f"https://proxy.golang.org/{pkg}/@v/list"
+    r = requests.get(url, timeout=5)
+    return r.status_code == 200
