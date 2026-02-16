5ce4446941e4cd5bc0601796f323cbd8f4cce946
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-02-16 11:03:32 +0100

    Update rubygems.py

--- konfusio/registries/rubygems.py
+++ konfusio/registries/rubygems.py
@@ -1,12 +1,6 @@
 import requests
-from .base import BaseRegistry
 
-class RubyGemsRegistry(BaseRegistry):
-    name = "rubygems"
-
-    def exists(self, package):
-        try:
-            r = requests.get(f"https://rubygems.org/api/v1/gems/{package}.json", timeout=5)
-            return r.status_code != 404
-        except:
-            return True
+def exists(pkg):
+    url = f"https://rubygems.org/api/v1/gems/{pkg}.json"
+    r = requests.get(url, timeout=5)
+    return r.status_code == 200
