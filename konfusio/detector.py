a55a1ab216c1e901127ae7a8e54b71a0df2a60b6
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-02-16 11:02:09 +0100

    Update detector.py

--- konfusio/detector.py
+++ konfusio/detector.py
@@ -1,31 +1,33 @@
-from konfusio.parsers import npm, composer, pypi, maven, golang, rubygems, nuget
+from konfusio.parsers.js_parser import parse_js
+from konfusio.parsers.npm_parser import parse_npm_manifest
+from konfusio.parsers.composer_parser import parse_composer
+from konfusio.parsers.requirements_parser import parse_requirements
+from konfusio.parsers.maven_parser import parse_maven
+from konfusio.parsers.go_parser import parse_go_mod
+from konfusio.parsers.gem_parser import parse_gemfile
+
 
 def detect_ecosystem(url, content):
-    if url.endswith("package.json"):
-        return "npm", npm.parse_package_json
-
-    if url.endswith("package-lock.json"):
-        return "npm", npm.parse_package_lock
-
-    if url.endswith("composer.json"):
-        return "packagist", composer.parse_composer
-
-    if url.endswith("requirements.txt"):
-        return "pypi", pypi.parse_requirements
-
-    if url.endswith("pom.xml"):
-        return "maven", maven.parse_pom
-
-    if url.endswith("go.mod"):
-        return "golang", golang.parse_go_mod
-
-    if url.endswith("Gemfile"):
-        return "rubygems", rubygems.parse_gemfile
-
-    if url.endswith(".csproj"):
-        return "nuget", nuget.parse_csproj
 
     if url.endswith(".js"):
-        return "npm", npm.parse_js
+        return "npm", parse_js
+
+    if url.endswith("package.json") or url.endswith("package-lock.json"):
+        return "npm", parse_npm_manifest
+
+    if url.endswith("composer.json"):
+        return "packagist", parse_composer
+
+    if url.endswith("requirements.txt"):
+        return "pypi", parse_requirements
+
+    if url.endswith("pom.xml"):
+        return "maven", parse_maven
+
+    if url.endswith("go.mod"):
+        return "golang", parse_go_mod
+
+    if url.endswith("Gemfile"):
+        return "rubygems", parse_gemfile
 
     return None, None
