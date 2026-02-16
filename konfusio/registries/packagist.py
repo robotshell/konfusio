08033567def87922c789a035c1399a3bb4fb4969
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-02-16 11:02:54 +0100

    Update packagist.py

--- konfusio/registries/packagist.py
+++ konfusio/registries/packagist.py
@@ -1,13 +1,6 @@
 import requests
-from .base import BaseRegistry
 
-class PackagistRegistry(BaseRegistry):
-    name = "packagist"
-
-    def exists(self, package):
-        try:
-            url = f"https://repo.packagist.org/p2/{package}.json"
-            r = requests.get(url, timeout=5)
-            return r.status_code != 404
-        except:
-            return True
+def exists(pkg):
+    url = f"https://repo.packagist.org/p2/{pkg}.json"
+    r = requests.get(url, timeout=5)
+    return r.status_code == 200
