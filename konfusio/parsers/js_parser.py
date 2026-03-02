d22ea1dbc240550a718e4efd680c04c9e607588f
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-03-02 09:48:49 +0100

    Update js_parser.py

--- konfusio/parsers/js_parser.py
+++ konfusio/parsers/js_parser.py
@@ -2,7 +2,8 @@ import re
 
 IMPORT_REGEX = [
     r'import\s+.*?\s+from\s+[\'"]([^\'"]+)[\'"]',
-    r'require\([\'"]([^\'"]+)[\'"]\)'
+    r'require\([\'"]([^\'"]+)[\'"]\)',
+    r'import\([\'"]([^\'"]+)[\'"]\)'
 ]
 
 def parse_js(content):
@@ -11,7 +12,12 @@ def parse_js(content):
     for pattern in IMPORT_REGEX:
         matches = re.findall(pattern, content)
         for m in matches:
-            if not m.startswith(".") and "/" not in m:
-                packages.add(m.strip())
+            m = m.strip()
+
+            # Exclude local/relative paths only
+            if m.startswith((".", "/")):
+                continue
+
+            packages.add(m)
 
     return packages
