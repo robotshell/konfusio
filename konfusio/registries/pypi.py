756f657f4da14947e9938dfd22595c4d12ecd38e
Robotshell <51330800+robotshell@users.noreply.github.com>
2026-03-12 08:59:34 +0100

    Update pypi.py

--- konfusio/registries/pypi.py
+++ konfusio/registries/pypi.py
@@ -2,5 +2,8 @@ import requests
 
 def exists(pkg):
     url = f"https://pypi.org/pypi/{pkg}/json"
-    r = requests.get(url, timeout=5)
-    return r.status_code == 200
+    try:
+        r = requests.get(url, timeout=5)
+        return r.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
